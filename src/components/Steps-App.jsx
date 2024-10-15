3cf6b587821decfa895b498826397179d15eed0b
emmeiwhite <emmeiwhite@gmail.com>
2024-10-15 20:18:49 +0530

    state --- UI (toggling)

--- src/components/Steps-App.jsx
+++ src/components/Steps-App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 export default function StepsApp() {
   const [step, setStep] = useState(1)
+  const [isOpen, setIsOpen] = useState(true)
 
   // Business Logic
   const handlePrev = () => {
@@ -18,25 +19,35 @@ export default function StepsApp() {
   }
 
   return (
-    <section className="steps-container">
-      <h1>Steps App</h1>
-      <div className={`steps-wrapper`}>
-        <div className={`step  ${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`step  ${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`step  ${step >= 3 ? 'active' : ''}`}>3</div>
+    <main>
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className="toggle"
+      >
+        {isOpen ? <span>❌</span> : <span>✅</span>}
       </div>
+      {isOpen && (
+        <section className="steps-container">
+          <h1>Steps App</h1>
+          <div className={`steps-wrapper`}>
+            <div className={`step  ${step >= 1 ? 'active' : ''}`}>1</div>
+            <div className={`step  ${step >= 2 ? 'active' : ''}`}>2</div>
+            <div className={`step  ${step >= 3 ? 'active' : ''}`}>3</div>
+          </div>
 
-      <h2>
-        <span>Step- {step}:</span>
-        {step === 1 && 'Learn React 💻'}
-        {step === 2 && 'Apply for Jobs 📝'}
-        {step === 3 && 'Invest your new income 🚀'}
-      </h2>
+          <h2>
+            <span>Step- {step}:</span>
+            {step === 1 && 'Learn React 💻'}
+            {step === 2 && 'Apply for Jobs 📝'}
+            {step === 3 && 'Invest your new income 🚀'}
+          </h2>
 
-      <div className="buttons-wrapper">
-        <button onClick={handlePrev}>Previous</button>
-        <button onClick={handleNext}>Next</button>
-      </div>
-    </section>
+          <div className="buttons-wrapper">
+            <button onClick={handlePrev}>Previous</button>
+            <button onClick={handleNext}>Next</button>
+          </div>
+        </section>
+      )}
+    </main>
   )
 }
